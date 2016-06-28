bdd694070d8ce949bd4c7023744edaf14842f60b
scopemedia <ewan@scopemedia.com.au>
2016-06-29 06:07:13 +1000

    Lesson 10 fetch exercise

---
+++ lesson-10-fetch-exercise/index.js
@@ -0,0 +1,56 @@
+/**
+ * Your forecast.io key is available as the global variable:
+
+process.env.FORECAST_KEY
+
+ *
+ */
+
+/**
+ * We include this line to ensure `fetch()` is ready to use in node
+ */
+require('isomorphic-fetch');
+
+
+// Your code here...
+
+// process.env.FORECAST_KEY
+// fetch('https://api.forecast.io/forecast/e6674f61c08ff2ca8d4890dc7919f4a6/-33.8678500,151.2073200').then((response) => {
+
+
+// 2 - Use the forecast.io API to get the current weather in your area
+
+function weather() {
+
+  fetch('https://api.forecast.io/forecast/' + process.env.FORECAST_KEY + '/' + lat + ',' + long + '?units=si').then((response) => {
+    return response.json()
+  }).then((dataAsJson) => {
+
+    console.log(dataAsJson);
+    console.log('At ' + dataAsJson.timezone + '\n' + 'The weather is ' + dataAsJson.hourly.summary + '\n' + 'Current temp is ' + dataAsJson.currently.temperature + ' degrees celcius');
+  }).catch(function(err) {
+    console.log('Error Forecast ', err);
+  });
+
+};
+
+
+
+// 1 - Use Google Maps to get Lat and Long
+  // Need to add error cacth
+
+
+fetch('http://maps.googleapis.com/maps/api/geocode/json?address=Redfern').then((response) => {
+  return response.json()
+}).then((dataAsJson) => {
+
+  // Store lat and long as variables - Look for the right object
+  lat = dataAsJson.results[0].geometry.location.lat;
+  long = dataAsJson.results[0].geometry.location.lng;
+
+  // Once we have the lat long co-ords call the weather function
+  weather();
+
+}).catch(function(err) {
+  console.log('Error GooleAPI ', err );
+});
